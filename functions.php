63afc2533761bf5810ccace235143cf692e2a168
unknown <pprus94@gmail.com>
2023-03-08 15:52:52 +0100

    campus maps

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files(){
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDFHh0p0AcmftqOftmWRnlqD_XTwkmT04g', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -92,7 +93,10 @@ function university_adjust_queries($query){
 
 add_action('pre_get_posts', 'university_adjust_queries');
 
-
+function universityMapKey($api){
+  $api['key'] = '';
+  return $api;
+}
 
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
